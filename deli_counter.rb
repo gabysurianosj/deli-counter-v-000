dcbfae642bee66058a9f994c9dbc343d9212c10e
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 11:05:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
       line_positions << "#{number}. #{customer}"
       number += 1 
     end 
-    puts "The line is currently: #{line_positions.join("")}"
+    puts "The line is currently: #{line_positions.join(" ")}"
   end 
 end 
 
